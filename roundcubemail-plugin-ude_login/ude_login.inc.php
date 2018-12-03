186d590bb0fd7f1bccdd4a47ec2393339e580938
LBS BuildBot <tp@tbits.net>
2018-12-03 19:24:41 +0100

    latest build for TBits

--- roundcubemail-plugin-ude_login/ude_login.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config['ude_use_grep'] = true;
-$config['ude_login_db'] = '/etc/roundcubemail/enable_disable_per_user.txt';
-#$config['ude_default_plugins'] = array('acl', 'managesieve');
-
-?>
