b776f771b3379f26bafa71f1a8fdd90281831d90
Timotheus Pokorra <tp@tbits.net>
2020-04-14 17:08:01 +0200

    upgrade packages to version from 31 March 2020

--- updatePackages/setup.sh
+++ updatePackages/setup.sh
@@ -2,18 +2,18 @@
 
 # create new package instructions
 
-RELEASE=107
+RELEASE=108
 obsbranch="Kolab_16"
 patchesbranch="Kolab16"
 branch="TBitsKolab16Dev"
 
-# version from March 2019
-githash=d3634b1746446874ec471653b1dae4e61cc0f076
+# version from 31 March 2019
+githash=0ff781639ce63d038edaacfa41b5be1530e34c20
 # version from July 2018
-githash_roundcubemail=4b631825f9afcfae3be3a806e2d31da3ee63180c
-githash_roundcubemail_plugins_kolab=$githash_roundcubemail
-githash_roundcubemail_skin_chameleon=$githash_roundcubemail
-githash_roundcubemail_plugin_contextmenu=$githash_roundcubemail
+#githash_roundcubemail=4b631825f9afcfae3be3a806e2d31da3ee63180c
+#githash_roundcubemail_plugins_kolab=$githash_roundcubemail
+#githash_roundcubemail_skin_chameleon=$githash_roundcubemail
+#githash_roundcubemail_plugin_contextmenu=$githash_roundcubemail
 
 TBITSPATCHESPATH=~/tmp/lbs-$branch/updatePackages
 PATCHESPATH=~/tmp/KolabScripts/kolab/patches
