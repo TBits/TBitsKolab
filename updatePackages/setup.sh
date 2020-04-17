f3c56e9650d0e7734c531ba5cbaed62c459fd532
Timotheus Pokorra <tp@tbits.net>
2020-04-17 16:56:17 +0200

    drop some patches for roundcube which are now part of upstream

--- updatePackages/setup.sh
+++ updatePackages/setup.sh
@@ -206,10 +206,6 @@ do
     then
       # from initTBitsISP.sh
       cp $PATCHESPATH/optional_disable_addressbook_export.patch .
-      cp $TBITSPATCHESPATH/backport_managesieve_forwards.patch .
-
-      # fix for the tests
-      cp $TBITSPATCHESPATH/roundcubemail_messages.patch .
     fi
 
     if [[ "$pkgname" == "cyrus-imapd" ]]
